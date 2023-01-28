440aae0fdfd5a095496f3cff573c37b18e03c7b8
Amir Mojiri <amojiry@gmail.com>
2023-01-28 15:18:37 +0200

    Add new ones

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230128.150958';
+const cacheName = 'chirpy-20230128.151443';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
