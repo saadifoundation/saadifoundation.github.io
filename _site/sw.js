cb16e7bf918685262c91cfcb27b87a110eb4fcab
Amir Mojiri <amojiry@gmail.com>
2023-01-28 17:31:10 +0200

    Undo home

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230128.185656';
+const cacheName = 'chirpy-20230128.185907';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
