341f441156509f8c6cc04fde55e484b42628def5
Amir Mojiri <amojiry@gmail.com>
2023-02-01 00:55:31 +0200

    Fix rtl problems

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -43,7 +43,7 @@ const resource = [
 const allowedDomains = [
     
 
-    'localhost:4000',
+    'localhost:4001',
 
     
 
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230130.142542';
+const cacheName = 'chirpy-20230201.022455';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
