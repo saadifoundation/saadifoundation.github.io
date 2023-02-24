e01961d19eb9b1c60abf4333456677796663e033
Amir Mojiri <amojiry@gmail.com>
2023-02-24 23:07:08 +0200

    Edit styles

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230224.234015';
+const cacheName = 'chirpy-20230225.003629';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
