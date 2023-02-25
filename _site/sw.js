1d31b6f46a12df25b9a44d405097db76514b0c10
Amir Mojiri <amojiry@gmail.com>
2023-02-25 13:14:45 +0200

    Samll fixes

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230225.003629';
+const cacheName = 'chirpy-20230225.144203';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
