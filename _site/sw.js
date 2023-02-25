265e0ba35d7f7e09033a7d193612a736d77e2331
Amir Mojiri <amojiry@gmail.com>
2023-02-25 13:38:33 +0200

    Prepare post image to be a column with contact detail

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230225.145627';
+const cacheName = 'chirpy-20230225.150708';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
