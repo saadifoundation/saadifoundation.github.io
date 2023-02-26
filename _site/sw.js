47f9433035296c3ea4fe89c29ac6942c28e1a99b
Amir Mojiri <amojiry@gmail.com>
2023-02-26 17:47:39 +0200

    Change styles

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230226.190829';
+const cacheName = 'chirpy-20230226.191346';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
