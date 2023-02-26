9f2030b870a3439d5df88e3a00460ddbada6f91c
Amir Mojiri <amojiry@gmail.com>
2023-02-26 17:38:53 +0200

    Change layout of register page

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230226.190647';
+const cacheName = 'chirpy-20230226.190829';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
