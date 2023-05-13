44da45363c02a206183a6aac8a5bdbc060d60630
Amir Mojiri <amojiry@gmail.com>
2023-05-13 18:04:49 +0300

    Publish website

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230228.022301';
+const cacheName = 'chirpy-20230513.183412';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
